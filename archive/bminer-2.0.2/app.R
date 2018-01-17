49cd2b8ad883b5d448f022919a45502be14f68d4
Laura Carroll <lmc297@cornell.edu>
2018-01-17 18:32:24 -0500

    Update app.R

--- archive/bminer-2.0.2/app.R
+++ archive/bminer-2.0.2/app.R
@@ -276,9 +276,9 @@ server <- function(input, output) {
     # virulence
     isolate.names<-lapply(lines,function(x) strsplit(x[1],split="BTyper Results for ")[[1]][2])
     if(task=="virulence"){
-      vgenes<-sapply(lines,function(x) which(vapply(x,function(r) grepl("\\|",r)&&!(grepl("rpoB\\|",r))&&!(grepl("\\(",r)),FUN.VALUE = 1)==1),simplify="list")}
+      vgenes<-sapply(lines,function(x) which(vapply(x,function(r) grepl("\\|",r)&&!(grepl("rpoB\\|",r))&&!(grepl("\\(",r)),FUN.VALUE = 1)==1),simplify=FALSE)}
     else if (task=="AMR"){
-      vgenes<-sapply(lines,function(x) which(vapply(x,function(r) grepl("\\|",r)&&!(grepl("rpoB\\|",r))&&(grepl("\\(",r)),FUN.VALUE = 1)==1), simplify="list")}
+      vgenes<-sapply(lines,function(x) which(vapply(x,function(r) grepl("\\|",r)&&!(grepl("rpoB\\|",r))&&(grepl("\\(",r)),FUN.VALUE = 1)==1), simplify=FALSE)}
     print("vgenes")
     print(input$tabs)
     if(length(unlist(vgenes))==0){
